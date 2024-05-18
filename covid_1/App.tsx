721b47e7a061e5890fb5e19b526f17bc020ccde1
SravaniManepalli <131232109+SravaniManepalli@users.noreply.github.com>
2024-05-18 15:55:31 +0530

    Create App.tsx

---
+++ covid_1/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css';
+import { Affected } from './Affected';
+import {Vaccinated} from './Vaccinated';
+function App() {
+  return (
+    <div className="App" id="container">
+      
+      <Affected />
+      <Vaccinated />
+
+  </div>
+  );
+}
+
+export default App;
